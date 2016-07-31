d5b06503866da1773477947ac3c791da0eba09b1
mishuagopian <mishuagopian@gmail.com>
2016-07-31 03:49:48 -0300

    Fixing graph and scripts

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,25 +1,8 @@
 var chart;
 var maxSpeed = 0;
+var GRAPH_VALUES = 100;
+
 $(document).ready(function() {
-  // var chartData = [{
-  //     values: [],
-  //     type: 'line',
-  //     yAxis: 1,
-  //     key: 'Deviation ',
-  //     color: '#ff7f0e'
-  // }, {
-  //     values: [],
-  //     type: 'line',
-  //     yAxis: 1,
-  //     key: 'Mod',
-  //     color: '#2ca02c'
-  // }, {
-  //     values: [],
-  //     type: 'line',
-  //     yAxis: 2,
-  //     key: 'Speed',
-  //     color: '#ff0000'
-  // }];
   var chartData = [{
       values: [],
       type: 'line',
@@ -31,26 +14,23 @@ $(document).ready(function() {
   var exampleSocket = new WebSocket("ws://localhost:8080");
   exampleSocket.onmessage = function (event) {
     var data = JSON.parse(event.data);
-      console.log(event.data);
-    // chartData[0].values.push({ x: data.timestamp, y: data.deviation });
-    // chartData[1].values.push({ x: data.timestamp, y: data.mod });
-    // chartData[2].values.push({ x: data.timestamp, y: data.speed });
+    console.log(event.data);
+
     chartData[0].values.push({ x: data.timestamp, y: data.speed });
-    if (chartData[0].values.length > 120) {
-      // chartData[0].values = chartData[0].values.slice(chartData[0].values.length - 120);
-      // chartData[1].values = chartData[1].values.slice(chartData[1].values.length - 120);
-      // chartData[2].values = chartData[2].values.slice(chartData[2].values.length - 120);
-      chartData[0].values = chartData[0].values.slice(chartData[0].values.length - 120);
+    if (chartData[0].values.length > GRAPH_VALUES) {
+      chartData[0].values = chartData[0].values.slice(chartData[0].values.length - GRAPH_VALUES);
     }
+
     maxSpeed = maxSpeed < data.speed ? data.speed : maxSpeed;
     d3.select('#chart svg')
         .datum(chartData)
         .transition().duration(500)
         .call(chart);
-    // chart.yDomain2([-1, maxSpeed]);
     chart.yDomain1([-1, maxSpeed]);
     chart.update(true);
+    $('.resting').replaceWith('<div class="resting">Resting ' + data.resting + '</div>');
   }
+
   nv.addGraph(function() {
       chart = nv.models.multiChart()
           .interpolate("basis")
@@ -58,12 +38,6 @@ $(document).ready(function() {
       chart.xAxis
           .axisLabel('Time')
           .tickFormat(function(d) { return d3.time.format('%M %S %L')(new Date(d)); });
-      // chart.yAxis1
-      //     .axisLabel('Acceleration (G)')
-      //     .tickFormat(d3.format('.02f'));
-      // chart.yAxis2
-      //     .axisLabel('Speed')
-      //     .tickFormat(d3.format('.02f'));
       chart.yAxis1
           .axisLabel('Speed')
           .tickFormat(d3.format('.02f'));
@@ -72,7 +46,7 @@ $(document).ready(function() {
           .transition().duration(500)
           .call(chart);
       nv.utils.windowResize(chart.update);
-      chart.height(400)
+      chart.height(400);
       return chart;
-  })
-})
+  });
+});
